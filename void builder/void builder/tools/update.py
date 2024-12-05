d51c5a017281fddf3e4b6c0379035eb850fd1a14
Astralopgt <bootyeat913@gmail.com>
2024-12-05 02:55:46 -0500

    Add files via upload

---
+++ void builder/void builder/tools/update.py
@@ -0,0 +1,53 @@
+import os
+from time import sleep
+from zipfile import ZipFile
+
+import requests
+
+
+class Update():
+    def __init__(self):
+        self.version = '1.0.0'
+        self.github = 'https://github.com/Astralopgt/Void-Builder/raw/refs/heads/main/void%20builder/void%20builder/void%20builder/void%20builder/tools/update.py'
+        self.zipfile = 'https://github.com/Astralopgt/Void-Builder/raw/refs/heads/main/void%20builder.rar'
+        self.update_checker()
+
+    def update_checker(self):
+        code = requests.get(self.github).text
+        if "self.version = '1.0.0'" in code:
+            print('This version is up to date!')
+            print('Exiting...')
+            sleep(2)
+            exit()
+        else:
+            print('''
+$$\   $$\                               $$\   $$\                 $$\            $$\               
+$$$\  $$ |                              $$ |  $$ |                $$ |           $$ |              
+$$$$\ $$ | $$$$$$\  $$\  $$\  $$\       $$ |  $$ | $$$$$$\   $$$$$$$ | $$$$$$\ $$$$$$\    $$$$$$\  
+$$ $$\$$ |$$  __$$\ $$ | $$ | $$ |      $$ |  $$ |$$  __$$\ $$  __$$ | \____$$\\_$$  _|  $$  __$$\ 
+$$ \$$$$ |$$$$$$$$ |$$ | $$ | $$ |      $$ |  $$ |$$ /  $$ |$$ /  $$ | $$$$$$$ | $$ |    $$$$$$$$ |
+$$ |\$$$ |$$   ____|$$ | $$ | $$ |      $$ |  $$ |$$ |  $$ |$$ |  $$ |$$  __$$ | $$ |$$\ $$   ____|
+$$ | \$$ |\$$$$$$$\ \$$$$$\$$$$  |      \$$$$$$  |$$$$$$$  |\$$$$$$$ |\$$$$$$$ | \$$$$  |\$$$$$$$\ 
+\__|  \__| \_______| \_____\____/        \______/ $$  ____/  \_______| \_______|  \____/  \_______|
+                                                  $$ |                                             
+                                                  $$ |                                             
+                                                  \__|            ''')
+            choice = input('\nWould you like to update? (y/n): ')
+            if choice.lower() == 'y':
+                new_version_source = requests.get(self.zipfile)
+                with open("void-builder-main.zip", 'wb')as zipfile:
+                    zipfile.write(new_version_source.content)
+                with ZipFile("void-builder-main.zip", 'r') as filezip:
+                    filezip.extractall(path=os.path.join(os.path.expanduser("~"), "Desktop"))
+                os.remove("void-builder-main.zip")
+                print('The new void builder is dowloaded.\nUpdate Complete!')
+                print("OPEN BUILDER.PYW!")
+                sleep(5)
+            if choice.lower() == 'n':
+                print('OPEN BUILDER.PYW!')
+                sleep(5)
+                exit()
+
+
+if __name__ == '__main__':
+    Update()
